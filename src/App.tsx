0ec476940e1c71d8e4be5bb43d8fa0c77341c050
Kyle Erikson <Kyle.Erikson@intelex.com>
2021-03-16 13:48:47 -0600

    Added react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_REPORTS = gql`
+  query getReports($managerId: Int) {
+    getReports(managerId: $managerId) {
+      id
+      name
+      email
+      manager_id
+    }
+  }
+`;
+
+function Reports() {
+  const { loading, error, data } = useQuery(GET_REPORTS, {
+    variables: { managerId: 1 },
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <p>`Error! ${error}`</p>;
+
+  return <p>{data.getReports.name}</p>;
+}
 
 function App() {
+  const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Reports />
+      </div>
+    </ApolloProvider>
   );
 }
 
